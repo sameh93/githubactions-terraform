c15f08613f0d4c41411223174f3ee2e6a5074c72
sameh93 <samehamen3393@gmail.com>
2022-06-14 17:01:45 +0200

    testing separation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,23 @@
 terraform {
-  cloud {
-    organization = "smamdouhlabs"
-
-    workspaces {
-      name = "github-actions-terraform"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      Version = "~>3.27"
     }
   }
+
+  required_version = ">=0.14.9"
+ backend "s3" {
+    bucket = "samen-github-actions"
+    key    = "tfstate/tfstate.tf"
+    region = "east-us-1"
+   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
-/*
+
 resource "aws_instance" "main_ec2" {
   count                       = 1
   ami                         = "ami-0022f774911c1d690"
@@ -20,4 +26,3 @@ resource "aws_instance" "main_ec2" {
   associate_public_ip_address = "true"
   vpc_security_group_ids      = ["sg-0d0f4148b2d8565a9"]
 }
-*/
